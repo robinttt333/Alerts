005eb93c2badd49b9a8346d20ebea21b8f3ea66a
Robin <robinttt333@gmail.com>
2020-08-12 12:02:00 +0530

    Reddit model updated for a single post

--- src/Alerts/reddit/models.py
+++ src/Alerts/reddit/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 class RedditPost(models.Model):
 	title = models.TextField()
+	body = models.TextField()
+	comments = models.IntegerField()
+	createdDate = models.DateField(auto_now_add = True)
+	url = models.TextField()
+	postId = models.CharField(max_length = 20)
 	
